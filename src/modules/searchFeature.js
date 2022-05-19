87d2741943a40eaf1a9089c9815cb105bf3a0e81
Emily <emilydta@users.noreply.github.com>
2022-05-19 19:41:48 +1000

    fix: search feature

--- src/modules/searchFeature.js
+++ src/modules/searchFeature.js
@@ -1,3 +1,25 @@
-const searchInput = document.getElementById('search');
-const submit = document.querySelector('.submit');
+import { displayWeatherData } from "./generateWeather";
+import { createBackground } from "./background";
 
+export const searchFunction = () => {
+    const search = document.getElementById('search');
+    const submit = document.getElementById('submit');
+
+    submit.addEventListener("click", () => {
+        createBackground(search.value);
+        displayWeatherData(search.value);
+    });
+
+    window.addEventListener('keydown', (e) => {
+        if (e.code === "Enter") {
+            createBackground(search.value);
+            displayWeatherData(search.value);
+        }
+    });
+
+    //prevent default form submit behaviour
+    document.querySelector(".location-form").onsubmit = function(e) {
+        e.preventDefault();
+    }
+
+}
